8b68d3106621d9f6a19bbf377f15d276805d2cc7
Riya Kumari <kumaririya1030@gmail.com>
2026-06-01 16:29:48 +0530

    Create script.js

---
+++ Practice Questions/project4.html/script.js
@@ -0,0 +1,10 @@
+
+let count = 0;
+function increase(){
+    count++;
+    document.getElementById("counter").innerHTML = count;
+}
+function decrease(){
+    count--;
+    document.getElementById("counter").innerHTML = count;
+}
